e11a35525b4d34f391de5953bfffe0b09280df90
Nandu <nanduruttala@gmail.com>
2022-09-10 19:48:50 +0530

    practice

---
+++ practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace practice
+{
+    class vote
+    {
+        public static void Main()
+        {
+            int age;
+            Console.WriteLine("enter age of a person");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("you are eligible to vote");
+            }
+            else
+            {
+                Console.WriteLine("you are not eligible to vote");
+            }
+        }
+    }
+        }
+    
+
